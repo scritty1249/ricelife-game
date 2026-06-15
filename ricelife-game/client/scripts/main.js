2f77f63098b9d055ed6833abe32d536450e90fff
Kyle <kaisonpham@gmail.com>
2026-06-15 01:46:34 -0700

    Adjusted explosion volume

--- ricelife-game/client/scripts/main.js
+++ ricelife-game/client/scripts/main.js
@@ -297,7 +297,7 @@ async function load() {
         const bassNode = AudioCtx.newBassNode();
         bassNode.frequency.value = 200;
         audioLayers.blast = AudioPlayer.Layer([bassNode]);
-        audioLayers.blast.volume = .4;
+        audioLayers.blast.volume = .55;
     }
     // resume audio context on inputs
     window.addEventListener("pointermove", AudioCtx.wake);
