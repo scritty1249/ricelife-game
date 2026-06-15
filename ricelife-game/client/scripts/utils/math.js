7f939ff9782c785fa178feee839aff3b6c074833
Kyle <kaisonpham@gmail.com>
2026-06-14 17:15:53 -0700

    Added function for averaging raw angles

--- ricelife-game/client/scripts/utils/math.js
+++ ricelife-game/client/scripts/utils/math.js
@@ -9,6 +9,19 @@ export function str2hex (str) {
     .join("");
 }
 
+export function averageAngle (radians = []) {
+  if (!radians || radians.length === 0) return 0;
+  let x = 0, y = 0;
+  for (const angle of radians) {
+    x += Math.cos(angle);
+    y += Math.sin(angle);
+  }
+  const count = radians.length;
+  let avg = Math.atan2(y / count, x / count);
+  if (avg < 0) avg += 2 * Math.PI; // clamp / normalize
+  return avg;
+}
+
 export function roundToPlace (num, precision = 2) { const place = 10**precision; return Math.round(num * place) / place }
 
 export function roundTo (num, precision = 1) { return Math.round(num * precision) / precision }
