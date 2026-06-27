2aef9d358367a01b751044c163879f16fd66485c
Kyle <kaisonpham@gmail.com>
2026-06-26 18:42:59 -0700

    Added new class to handling drawing hitpoint bars onscreen

--- ricelife-game/client/scripts/game/player/hitamount.js
+++ ricelife-game/client/scripts/game/player/hitamount.js
@@ -1,4 +1,5 @@
-import { clamp, roundToPlace } from "../utils/utils.js";
+import { clamp, roundToPlace, floatEqual } from "../utils/utils.js";
+import { Color, Vector } from "../geometry/geometry.js";
 
 // parent class for health, shields, etc.
 // final stored value of anything should never be a decimal. Clamp or round instead of throwing error
@@ -24,12 +25,14 @@ export class HitAmount {
     #reserve = 0; // can be used to reduce maximum amount. May be a decimal. Cannot be more than max.
     #max;
     #amount;
+    #bar;
     constructor (max) {
         // [!] idiot proofing
         // throw error here instead of rounding silently. Max should be exact on init
         if (!Number.isFinite(max) || !Number.isInteger(max) || !max) throw new Error(`[${this.constructor.name}]: Hit maximum must be a positive, finite integer`);
         this.#max = max;
         this.#amount = max;
+        this.#bar = new HitAmountBar(this);
     }
 
     // methods that modify values should apply modifiers
@@ -90,9 +93,10 @@ export class HitAmount {
     }
 
     get isHitAmount () { return true }
-    get percent () { return this.amount / this.max }
+    get percentage () { return this.amount / this.max }
     get isZero () { return this.amount === 0 } // amount should be an Integer
     get limit () { return this.max - this.reserve }
+    get bar () { return this.#bar }
     get baseRegeneration () { return this.#baseRegeneration }
     set baseRegeneration (value) {
         // value is not rounded or limited
@@ -144,4 +148,80 @@ export class HitAmount {
     }
     get amount () { return this.#amount }
     set amount (value) { return (this.#amount = clamp(this.constructor.ROUNDING_FN(value), 0, this.limit)) }
+}
+
+export class HitAmountBar {
+    #fillColor = new Color(0, 255, 0, 1);
+    #fillPattern = undefined;
+    #emptyColor = new Color(0, 0, 0, 0);
+    #emptyPattern = undefined;
+    #size = new Vector();
+    #hitamount;
+    blend = false; // whether to draw empty color and pattern below fill, or draw the two seperately
+    constructor (hitamount) {
+        if (!hitamount?.isHitAmount) throw new Error(`[${this.constructor.name}]: Invalid parameter - expected HitAmount, got ${typeof hitamount}`);
+        this.#hitamount = hitamount;
+    }
+
+    drawEmpty (cursor, x, y, dx, dy) {
+        cursor.save();
+        if (!floatEqual(this.emptyColor.A, 0)) {
+            cursor.fillStyle = this.emptyColor.toString();
+            cursor.fillRect(x, y, dx, dy);
+        }
+        if (this.emptyPattern !== undefined) {
+            cursor.fillStyle = this.emptyPattern;
+            cursor.fillRect(x, y, dx, dy);
+        }
+        cursor.restore();
+    }
+    drawFilled (cursor, x, y, dx, dy) {
+        cursor.save();
+        if (!floatEqual(this.fillColor.A, 0)) {
+            cursor.fillStyle = this.fillColor.toString();
+            cursor.fillRect(x, y, dx, dy);
+        }
+        if (this.fillPattern !== undefined) {
+            cursor.fillStyle = this.fillPattern;
+            cursor.fillRect(x, y, dx, dy);
+        }
+        cursor.restore();
+    }
+    draw (cursor, position) {
+        if (this.size.isZero) return;
+        // calculate rectangles
+        const halfWidth = this.width / 2;
+        const halfHeight = this.height / 2;
+        const fillWidth = this.width * this.#hitamount.percentage;
+
+        const startX = position.x - halfWidth;
+        const startY = position.y - halfHeight;
+        const midX = startX + fillWidth;
+        const endX = position.x + halfWidth;
+        const endY = position.y + halfHeight;
+
+        this.drawEmpty(
+            cursor,
+            this.blend ? startX : midX, startY,
+            endX, endY
+        );
+        this.drawFilled(
+            cursor,
+            startX, startY,
+            midX, endY
+        );
+    }
+
+    get isHitAmountBar () { return true }
+    get fillColor () { return this.#fillColor }
+    get fillPattern () { return this.#fillPattern }
+    set fillPattern (pattern) { return (this.#fillPattern = pattern) }
+    get emptyColor () { return this.#emptyColor }
+    get emptyPattern () { return this.#emptyPattern }
+    set emptyPattern (pattern) { return (this.#emptyPattern = pattern) }
+    get size () { return this.#size }
+    get width () { return this.size.x }
+    set width (pixels) { return (this.size.x = pixels) }
+    get height () { return this.size.y }
+    set height (pixels) { return (this.size.y = pixels) }
 }
