8b9704e8ab06965129fd0f28a06d7cd7506a17e3
Kyle <kaisonpham@gmail.com>
2026-07-20 03:42:32 -0700

    Fixed terrain highlight rendering bugs

--- ricelife-game/client/scripts/game/terrain/create.js
+++ ricelife-game/client/scripts/game/terrain/create.js
@@ -7,24 +7,27 @@ export function createTerrain (dataIterator) {
     plane.max.apply(dataIterator.next().value, dataIterator.next().value);
     let result = dataIterator.next();
     let maxX = result.value;
+    let maxY = 0;
     while (!result.done) {
         const x = result.value;
         if (x > maxX) maxX = x;
         result = dataIterator.next();
         const y = result.value;
+        if (y > maxY) maxY = y;
         path.push(new Vector(x, y));
         result = dataIterator.next();
     }
-    const rightBound = new Vector(maxX, 0);
-    const leftBound = new Vector(0, 0);
+    const rightBound = new Vector(maxX, -maxY / 2);
+    const leftBound = new Vector(0, -maxY / 2);
 
-    const p1 = path.at(-2);
-    const p2 = path.at(-1);
+    const p1 = path.at(-2).clone();
+    const p2 = path.at(-1).clone();
 
     if (!p1.eq(rightBound)) path.push(rightBound);
     if (!p2.eq(leftBound)) path.push(leftBound);
 
     const terrain = new Polygon(path);
+    terrain.subsection(0.5);
     terrain.userData.collision = Properties.DESTRUCTION | Properties.ENTER | Properties.TERRAIN;
     return { plane, terrain };
 };
--- ricelife-game/client/scripts/game/terrain/draw.js
+++ ricelife-game/client/scripts/game/terrain/draw.js
@@ -9,7 +9,6 @@ export function drawTerrain (cursor, polygon, fillColor, edgeColor, gradientWidt
     if (polygon.holes) {
         cursor.globalCompositeOperation = "destination-out";
         cursor.fillStyle = "black"; // mask color, abitrary
-        
         for (const hole of polygon.holes) {
             hole.draw(cursor);
             cursor.fill();
@@ -19,32 +18,6 @@ export function drawTerrain (cursor, polygon, fillColor, edgeColor, gradientWidt
         cursor.globalCompositeOperation = "source-over";
     }
 
-    const topEdge = polygon.path.clone();
-    {
-        // trim off the bottom (opening the path)
-        const yMax = Math.max(...topEdge.points.map((pt) => pt.y));
-        const pt1 = topEdge.points.findIndex((pt) => pt.y == yMax);
-        const pt2 = topEdge.points.findLastIndex((pt) => pt.y == yMax);
-        topEdge.splice(pt1, 1);
-        topEdge.splice(pt2 - 1, 1);
-        topEdge.apply(
-            ...topEdge.points.toSorted((a, b) => a.x - b.x)
-        );
-    }
-
-    const holeTopEdges = [];
-    if (polygon.holes) {
-        for (const hole of polygon.holes) {
-            const holeEdge = hole.path.clone();
-            const yMax = Math.max(...holeEdge.points.map((pt) => pt.y));
-            holeEdge.apply(...holeEdge.points.filter(pt => pt.y < yMax - 5)); 
-            if (holeEdge.points.length > 1) {
-                holeEdge.points.sort((a, b) => a.x - b.x);
-                holeTopEdges.push(holeEdge);
-            }
-        }
-    }
-
     cursor.save();
     polygon.draw(cursor);
     cursor.clip();
@@ -52,9 +25,9 @@ export function drawTerrain (cursor, polygon, fillColor, edgeColor, gradientWidt
     cursor.filter = `blur(${resolution}px)`;
     cursor.lineWidth = gradientWidth;
     cursor.strokeStyle = edgeColor.toRGBA();
-    topEdge.draw(cursor);
-    for (const openEdge of holeTopEdges)
-        openEdge.draw(cursor);
+    for (const edge of polygon.edges)
+        edge.draw(cursor);
+    cursor.stroke();
     cursor.restore();
     cursor.restore();
 }
