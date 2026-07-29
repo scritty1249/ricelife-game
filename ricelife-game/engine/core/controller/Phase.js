dd0d8e6f032950c6519305db18f90d7d6590ccf2
Kyle <kaisonpham@gmail.com>
2026-07-29 16:27:48 -0700

    Added menu support to Phase parent class - Phases must now register update actions within ontick and onanimate, instead of tick and animate

--- ricelife-game/engine/core/controller/Phase.js
+++ ricelife-game/engine/core/controller/Phase.js
@@ -4,9 +4,10 @@ import { Loop } from "./loop/Loop.js";
 import { Camera } from "./display/Camera.js";
 
 // exists within Main loop only
+// uses ontick and onanimate instead of tick and animate
 export class Phase extends Loop {
     static INPUT_MAP = {};
-    #Menus = {};
+    #Menus = new Map();
     #Interface = new PointerInterface();
     #Plane = new BoundingBox();
     #Camera;
@@ -19,16 +20,41 @@ export class Phase extends Loop {
             this.Camera.Viewbox.setPlane(this.Plane);
         });
     }
-    animate (clear = true) {}
+    drawBackground () {}
+    onanimate () {}
+    animate (clear = true) {
+        if (clear) this.Global.Display.cursor.clear();
+        const openMenus = this.Menus
+            .values()
+            .filter(({isOpen}) => isOpen);
+        if (openMenus?.length) {
+            this.drawBackground();
+            for (const menu of openMenus)
+                menu.animate(false);
+        } else
+            this.onanimate();
+    }
     start () {
         this.state = this.constructor.STATES.Ready;
     }
     reset () {
         this.state = this.constructor.STATES.Busy;
     }
+    async ontick (delta) {}
+    async tick (delta) {
+        super.tick(delta);
+        const openMenus = this.Menus
+            .values()
+            .filter(({isOpen}) => isOpen);
+        if (openMenus?.length)
+            await Promise.all(openMenus.map((menu) => menu?.tick?.(delta)));
+        else
+            await this.ontick?.(delta);
+    }
     get isPhase () { return true }
     get Global () { return this.#Global }
     get Interface () { return this.#Interface }
     get Plane () { return this.#Plane }
     get Camera () { return this.#Camera }
+    get Menus () { return this.#Menus }
 }
