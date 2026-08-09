f9f8ea7de913dbcdbe9672860c18ef2f08fb53ce
Kyle <kaisonpham@gmail.com>
2026-08-08 23:55:31 -0700

    Ported over Pine

--- tests/new-engine/game.js
+++ tests/new-engine/game.js
@@ -14,6 +14,7 @@ const lobby = {
                     "gigabouncer",
                     "digger",
                     "flower",
+                    "pine",
                 ],
                 "model": "basic",
                 "profile": {
