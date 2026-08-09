93ba17c9219b94c68e08200d9d41cbca2535e141
Kyle <kaisonpham@gmail.com>
2026-08-08 17:59:41 -0700

    Updated onResize handler to be accessible from subclasses

--- ricelife-game/engine/core/menu/item/ScreenButton.js
+++ ricelife-game/engine/core/menu/item/ScreenButton.js
@@ -10,13 +10,15 @@ export class ScreenButton extends Button {
         super();
         if (!appCanvas?.isAppCanvas) throw new Error(`[${typeString(this)}]: Expected AppCanvas, got ${typeString(appCanvas)}`);
         this.#appCavnas = appCanvas;
-        this.#appCavnas.addResizeListener(this.#onResize);
-        this.#onResize();
+        this.#appCavnas.addResizeListener(this.#resizeHandler);
+        this.onResize();
     }
 
-    #onResize = () => { this.#bbox.max.apply(this.#appCavnas.size) }
+    #resizeHandler = () => { this.onResize() }
+
+    onResize () { this.#bbox.max.apply(this.#appCavnas.size) }
     isOver (point) { return this.#bbox.isIntersecting(point) }
-    close () { this.#appCavnas.removeResizeListener(this.#onResize) }
+    close () { this.#appCavnas.removeResizeListener(this.#resizeHandler) }
 
     get isScreenButton () { return true }
 }
