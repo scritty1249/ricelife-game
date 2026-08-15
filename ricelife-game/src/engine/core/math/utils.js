de0d5dfef3171d6163491ff1829d1eb5edced58a
Kyle <kaisonpham@gmail.com>
2026-08-15 14:06:19 -0700

    Added utility function for radians

--- ricelife-game/src/engine/core/math/utils.js
+++ ricelife-game/src/engine/core/math/utils.js
@@ -1,3 +1,5 @@
+export const TWO_PI = 2 * Math.PI;
+
 export function clamp (num, min, max) { return Math.min(max, Math.max(min, num)) }
 
 export function equals (a, b) { return Math.abs(a - b) < Number.EPSILON }
@@ -6,3 +8,6 @@ export function round (num, precision = 1) {
     const multiplier = Math.pow(10, precision);
     return Math.round((num + Number.EPSILON) * multiplier) / multiplier;
 }
+
+// [!] modulo operators corrupt values (floating point errors) that can interfere with equality checks
+export function wrapAngle (radians) { return (radians % TWO_PI + TWO_PI) % TWO_PI }
