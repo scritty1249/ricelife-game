3cc206fcd5bf7ebab5b18d05324cfa6a4061eff0
Kyle <kaisonpham@gmail.com>
2026-08-17 03:22:18 -0700

    Added new button type

--- ricelife-game/src/engine/core/menu/item/Core.js
+++ ricelife-game/src/engine/core/menu/item/Core.js
@@ -2,3 +2,4 @@ export { Icon } from "./Icon.js";
 export { IconButton } from "./IconButton.js";
 export { ShapeButton } from "./ShapeButton.js";
 export { ScreenButton } from "./ScreenButton.js";
+export { ToggleIconButton } from "./ToggleIconButton.js";
