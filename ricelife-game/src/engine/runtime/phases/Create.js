28a5805a7040ea12376ba7bf9ef7b66d581e418b
Kyle <kaisonpham@gmail.com>
2026-08-25 16:59:12 -0700

    Partial implementation of Create phase

--- ricelife-game/src/engine/runtime/phases/Create.js
+++ ricelife-game/src/engine/runtime/phases/Create.js
@@ -5,7 +5,8 @@ import {
     ShapeButton,
     Circle,
     Color,
-    Label
+    Label,
+    HexaButton
 } from "../../core/Core.js";
 import { MapSelect } from "../menus/MapSelect.js";
 import { drawMenuItemRulers } from "../debug/draw.js";
@@ -38,6 +39,8 @@ import { drawMenuItemRulers } from "../debug/draw.js";
 //         const { menu } = this.store;
 //         if (!menu?.isOpen) this.Camera.update();
 //     }
+//     async ontick (delta) {
+//     }
 //     start () {
 //         // attach per start, consume on trigger. Prevent multiple map loads from being queued up at the same time
 //         this.store.menu.Events.addEventListener("SELECTED", (data) => this.#onMapSelected(data), Create.#selectedCallbackOptions);
@@ -55,59 +58,117 @@ export class Create extends Phase {
         super(mainController);
         this.#init(maps);
         this.#load()
+            .then(() => this.onResize())
             .then(() => this.resolveLoad())
             .catch((err) => this.rejectLoad(err));
     }
 
     #init (maps) {
-        const buttons = new ItemLayout();
-        {
-            const label = new Label("Teams", 50, undefined, "serif", this.Global.Display.cursor);
-            const slider = new Slider(new ShapeButton(new Circle(20), new Color(0, 0, 0, 1)), 10);
-            slider.dotOffset.apply(slider.dot.width / 2, slider.dot.height / 2);
-            slider.barWidth = 100;
-            slider.barHeight = 20;
-            buttons.push(label, slider);
-        }
-        {
-            const slider = new Slider(new ShapeButton(new Circle(20), new Color(0, 0, 0, 1)), 10);
-
-            slider.dotOffset.apply(slider.dot.width / 2, slider.dot.height / 2);
-            slider.barWidth = 100;
-            slider.barHeight = 20;
-            slider.barCornerRadius = 5;
-            buttons.push(slider);
-        }
-        {
-            const slider = new Slider(new ShapeButton(new Circle(20), new Color(0, 0, 0, 1)), 10);
-
-            slider.dotOffset.apply(slider.dot.width / 2, slider.dot.height / 2);
-            slider.barWidth = 500;
-            slider.barHeight = 20;
-            slider.barCornerRadius = 10;
-            buttons.push(slider);
-        }
-
-        buttons.isColumn = true;
-        buttons.gap = 15;
-        const { center } = this.Global.Display;
-        buttons.setPosition(center.x - (buttons.width / 2), center.y + (buttons.height / 2));
-        this.Interface.insert()
-            .push(buttons)
-            .fixed = true;
-        this.store.buttons = buttons;
+        this.#setupInterface();
         this.Plane.max.apply(1000, 1000);
         this.Camera.Viewbox.bounding.left = this.Camera.Viewbox.bounding.right = false;
     }
     async #load () {
+        
+    }
+    #setupInterface () {
+        const buttons = new ItemLayout();
+        buttons.isColumn = true;
+        buttons.gap = 15;
+        buttons.padding.apply(10);
+        this.Interface.insert()
+            .push(buttons)
+            .fixed = true;
+        this.store.buttons = buttons;
+        this.store.styling = {
+            dotColor: new Color(0, 0, 0, 1),
+            dotRadius: 12,
+            barColor: new Color(255, 255, 255, 1),
+            barWidth: 250,
+            barHeight: 8,
+            barCornerRadius: 4,
+            rowGap: 5
+        };
+        // [!] minimums on init
+        this.store.values = {
+            teamCount: 2,
+            teamSize: 1,
+        };
+        this.#setupMapOptions();
+        this.#setupTeamCountSlider();
+        this.#setupTeamSizeSlider();
+        this.#setupSubmitButton();
+    }
+    #setupMapOptions () {
+
+    }
+    #setupTeamCountSlider () {
+        const { DEFAULT_FONT, FONT_SIZE } = this.Global.store;
+        const { styling } = this.store;
+        const label = new Label("Teams", FONT_SIZE * 2, undefined, DEFAULT_FONT.family, this.Global.Display.cursor);
+        const row = new ItemLayout();
+        const slider = new Slider(new ShapeButton(new Circle(styling.dotRadius), styling.dotColor), 4, this.store.values.teamCount);
+        const value = new Label(slider.value, FONT_SIZE * 1.5, undefined, DEFAULT_FONT.family, this.Global.Display.cursor);
+        slider.dotOffset.apply(slider.dot.width / 2, slider.dot.height / 2);
+        slider.barColor.apply(styling.barColor);
+        slider.barWidth = styling.barWidth;
+        slider.barHeight = styling.barHeight;
+        slider.barCornerRadius = styling.barCornerRadius;
+        slider.onchange = () => value.text = this.store.values.teamCount = slider.value;
+        row.gap = styling.rowGap;
+        row.isColumn = false;
+        row.push(slider, value);
+        this.store.buttons.push(label, row);
+    }
+    #setupTeamSizeSlider () {
+        const { DEFAULT_FONT, FONT_SIZE } = this.Global.store;
+        const { styling } = this.store;
+        const label = new Label("Team Size", FONT_SIZE * 2, undefined, DEFAULT_FONT.family, this.Global.Display.cursor);
+        const row = new ItemLayout();
+        const slider = new Slider(new ShapeButton(new Circle(styling.dotRadius), styling.dotColor), 5, this.store.values.teamSize);
+        const value = new Label(slider.value, FONT_SIZE * 1.5, undefined, DEFAULT_FONT.family, this.Global.Display.cursor);
+        slider.dotOffset.apply(slider.dot.width / 2, slider.dot.height / 2);
+        slider.barColor.apply(styling.barColor);
+        slider.barWidth = styling.barWidth;
+        slider.barHeight = styling.barHeight;
+        slider.barCornerRadius = styling.barCornerRadius;
+        slider.onchange = () => value.text = this.store.values.teamSize = slider.value;
+        row.gap = styling.rowGap;
+        row.isColumn = false;
+        row.push(slider, value);
+        this.store.buttons.push(label, row);
+    }
+    #setupSubmitButton () {
+        const { DEFAULT_FONT, FONT_SIZE } = this.Global.store;
+        const { styling } = this.store;
+        const button = new HexaButton(styling.dotRadius * 2, styling.barWidth * (1/4));
+        const { width, height } = button.getBoundingBox();
+        button.fontSize = FONT_SIZE;
+        button.fontFamily = DEFAULT_FONT.family;
+        button.originOffset.apply(-width / 2, height / 2);
+        button.fillColor.apply(255, 255, 255, 1);
+        button.fontColor.apply(0, 0, 0, 1);
+        button.text = "Submit";
+        button.onclick = () => {
+            console.log(this.store.values.teamCount, this.store.values.teamSize);
+        }
+        this.store.buttons.push(button);
+    }
+    #drawDebugOverlay () {
+        const { cursor } = this.Global.Display;
+        drawMenuItemRulers(cursor, this.store.buttons, true, true);
     }
 
     onanimate () {
         const { cursor } = this.Global.Display;
         this.Interface.draw(cursor);
-        // [!] debug
-        for (const item of this.store.buttons)
-            drawMenuItemRulers(cursor, item, true);
-        drawMenuItemRulers(cursor, this.store.buttons, true);
+        if (this.Global.flags.DEBUG) this.#drawDebugOverlay();
+    }
+    onResize () {
+        const { center } = this.Global.Display;
+        const { buttons } = this.store;
+        buttons.setPosition(center.x - (buttons.width / 2), center.y + (buttons.height / 2));
+    }
+    async ontick (delta) {
     }
 }
