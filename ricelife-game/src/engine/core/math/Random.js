99984fe083b54b3d3196783b385cd3127b81f2c4
Kyle <kaisonpham@gmail.com>
2026-08-31 20:48:40 -0700

    Added method to generate a random seed from string

--- ricelife-game/src/engine/core/math/Random.js
+++ ricelife-game/src/engine/core/math/Random.js
@@ -4,11 +4,23 @@ export class Random {
     static #generateSeedPart () { return (Math.random()*2**32)>>>0 }
     static seed () {
         return [
-        Random.#generateSeedPart(),
-        Random.#generateSeedPart(),
-        Random.#generateSeedPart(),
-        Random.#generateSeedPart()
+            Random.#generateSeedPart(),
+            Random.#generateSeedPart(),
+            Random.#generateSeedPart(),
+            Random.#generateSeedPart()
         ]
+    }
+    // cyrb128
+    static seedString (str) {
+        let k, h1 = 1779033703, h2 = 3024734485, h3 = 3362625948, h4 = 502494325;
+        for (let i = 0; i < str.length; i++) {
+            k = str.charCodeAt(i);
+            h1 = h2 ^ Math.imul(h1 ^ k, 597399067);
+            h2 = h3 ^ Math.imul(h2 ^ k, 2869860233);
+            h3 = h4 ^ Math.imul(h3 ^ k, 951274213);
+            h4 = h1 ^ Math.imul(h4 ^ k, 2716044179);
+        }
+        return [h1 >>> 0, h2 >>> 0, h3 >>> 0, h4 >>> 0];
     }
     #seedA;
     #seedB;
