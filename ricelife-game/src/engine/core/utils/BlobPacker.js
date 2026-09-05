853fcbdd708fd067032024c86f171091e8a9dbca
Kyle <kaisonpham@gmail.com>
2026-09-04 18:51:07 -0700

    Optimized packing method and fixed bugs

--- ricelife-game/src/engine/core/utils/BlobPacker.js
+++ ricelife-game/src/engine/core/utils/BlobPacker.js
@@ -4,15 +4,16 @@ export class BlobPacker {
         const { HEADER_SIZE_OFFSET } = BlobPacker;
         
         const originalOffset = data.byteOffset || 0;
-        const originalLength = data.length || data.byteLength;
         const buffer = ArrayBuffer.isView(data) ? data.buffer : data;
 
-        const localLength = Number.isInteger(byteLength) && byteLength >= 0
-            ? byteLength : originalLength;
         const localOffset = Number.isInteger(byteOffset) && byteOffset >= 0
             ? byteOffset : 0;
+        const localLength = Number.isInteger(byteLength) && byteLength >= 0
+            ? byteLength : (ArrayBuffer.isView(data)
+                ? data.byteLength
+                : data.byteLength - localOffset);
 
-        const view = new DataView(buffer, originalOffset + localOffset, Math.min(localLength, originalLength));
+        const view = new DataView(buffer, localOffset + originalOffset, localLength);
         let offset = 0;
         while (offset < view.byteLength) {
             const length = view.getUint32(offset, true);
@@ -24,12 +25,30 @@ export class BlobPacker {
     }
     // close buffer if browser supports it
     static closeBuffer (buffer) {
-        if (typeof buffer?.transfer === "function")
-            buffer.transfer();
+        if (buffer instanceof ArrayBuffer && typeof buffer?.transfer === "function")
+            try { buffer.transfer(0) } catch {}
+    }
+    static #parseItem (item, textEncoder) {
+        if (item instanceof ArrayBuffer) {
+            return new Uint8Array(item);
+        } else if (ArrayBuffer.isView(item)) {
+            return new Uint8Array(item.buffer, item.byteOffset, item.byteLength);
+        } else if (item) {
+            return textEncoder.encode(JSON.stringify(item));
+        } else {
+            return new Uint8Array(0);
+        }
     }
     #textEncoder = new TextEncoder();
     #items = new Array();
     constructor () {}
+
+    #getDataViews () {
+        const views = [];
+        for (const item of this.#items)
+            views.push(BlobPacker.#parseItem(item, this.#textEncoder));
+        return views;
+    }
 
     // data may be an ArrayBuffer or JSON Object
     push (data) {
@@ -39,41 +58,22 @@ export class BlobPacker {
         return this.#items.at(index);
     }
     pack () {
-        const { HEADER_SIZE_OFFSET, closeBuffer } = BlobPacker;
-        const { buffers, length: totalItems} = this;
-        const size = buffers.reduce((acc, curr) => acc + curr.byteLength + HEADER_SIZE_OFFSET, 0);
+        const { HEADER_SIZE_OFFSET } = BlobPacker;
+        const views = this.#getDataViews();
+        const size = views.reduce((acc, curr) => acc + curr.byteLength + HEADER_SIZE_OFFSET, 0);
         const uintView = new Uint8Array(size);
         const dataView = new DataView(uintView.buffer);
         let offset = 0;
-        for (let i = 0; i < buffers.length && offset < size; i++) {
-            const buffer = buffers[i];
-            const length = buffer.byteLength;
+        for (const view of views) {
+            const length = view.byteLength;
             dataView.setUint32(offset, length, true);
             offset += HEADER_SIZE_OFFSET;
-            uintView.set(new Uint8Array(buffer), offset);
+            uintView.set(view, offset);
             offset += length;
-            closeBuffer(buffer);
         }
         return uintView.buffer;
     }
 
     get isBlobPacker () { return true }
     get length () { return this.#items.length }
-    // returns buffer clones
-    get buffers () {
-        const buffers = [];
-        for (const item of this.#items) {
-            if (item instanceof ArrayBuffer) {
-                buffers.push(item.slice(0));
-            } else if (ArrayBuffer.isView(item) && item?.buffer instanceof ArrayBuffer) {
-                buffers.push(item.buffer.slice(item.byteOffset, item.byteOffset + item.byteLength));
-            } else if (item) {
-                const buffer = this.#textEncoder.encode(JSON.stringify(item));
-                buffers.push(buffer);
-            } else {
-                buffers.push(new ArrayBuffer(0, {maxByteLength: 0}));
-            }
-        }
-        return buffers;
-    }
 }
