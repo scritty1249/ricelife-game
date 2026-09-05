2beed099dd2afa9547a2d34898597bc7073b17f8
Kyle <kaisonpham@gmail.com>
2026-09-04 21:01:27 -0700

    Removed unused import

--- ricelife-game/src/engine/core/projectile/blast/Blast.js
+++ ricelife-game/src/engine/core/projectile/blast/Blast.js
@@ -1,5 +1,4 @@
 import { Shape } from "../../geometry/Shape.js";
-import { Vector } from "../../math/Vector.js";
 import { Color } from "../../math/Color.js";
 import { typeString } from "../../utils/logging.js";
 
