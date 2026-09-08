a02c95bc8018533bfef68a2c22cced6673010d1f
Kyle <kaisonpham@gmail.com>
2026-09-08 15:48:01 -0700

    Removed unused JSON representation property

--- ricelife-game/src/engine/core/player/hitpoints/HitPoints.js
+++ ricelife-game/src/engine/core/player/hitpoints/HitPoints.js
@@ -99,8 +99,7 @@ export class HitPoints extends Hashable {
             amount: this.amount,
             regen: round(this.regeneration, 3),
             max: this.max,
-            reserve: round(this.reserve, 3),
-            hash: this.hash
+            reserve: round(this.reserve, 3)
         }
     }
 
