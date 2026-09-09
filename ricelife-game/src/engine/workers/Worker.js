0356b607e7025320682e93286bb2c5f7b9175696
Kyle <kaisonpham@gmail.com>
2026-09-08 19:32:01 -0700

    Added debug message

--- ricelife-game/src/engine/workers/Worker.js
+++ ricelife-game/src/engine/workers/Worker.js
@@ -136,6 +136,7 @@ self.onmessage = async (e) => {
             const result = AmmoMap.trace((await AMMO_TYPES.onready(ammoImport)), params, increment, limit, terrainCollider, colliders);
             if (!result.finished) console.debug(`${CONSOLE_PREFIX}: Trace operation timed out in Transaction ${id}`);
             const map = result.encode();
+            console.log(result, map);
             postResponse(id, map, map.buffers);
         } else if (type === "CUTTERRAIN") {
             /* Payload expected:
